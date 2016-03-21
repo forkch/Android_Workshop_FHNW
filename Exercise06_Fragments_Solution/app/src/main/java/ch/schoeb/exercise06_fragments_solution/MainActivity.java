c78abb0810ea9eec9ec05840529b3381a2c08dd9
Benjamin Müller <news@fork.ch>
2016-03-21 11:07:56 +0100

    new IDEA / AS files

--- Exercise06_Fragments_Solution/app/src/main/java/ch/schoeb/exercise06_fragments_solution/MainActivity.java
+++ Exercise06_Fragments_Solution/app/src/main/java/ch/schoeb/exercise06_fragments_solution/MainActivity.java
@@ -1,7 +1,6 @@
 package ch.schoeb.exercise06_fragments_solution;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
